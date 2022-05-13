8843461ed409bed735ff7c55d93eb5345af2455f
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-05-13 04:00:09 +0000

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,19 +1,44 @@
 sudo apt update
 sudo apt install nginx
-sudo systemctl status nginx.service
 sudo systemctl enable  nginx.service
 sudo apt -y install php7.4 php7.4-gd php7.4-mysql php7.4-zip php7.4-fpm
 wget https://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
 sudo cp -r wordpress/* /var/www/html
 sudo chown -R www-data:www-data /var/www/html
-sudo sed -i 's/index.html/index.php/' /etc/nginx/sites-available/default
-sudo sed -i 's/=404/index.php?$args/' /etc/nginx/sites-available/default
-sudo sed -i 's/server/#/' /etc/nginx/sites-available/default
-sudo tee -a /etc/nginx/sites-available/default > /dev/null <<EOT 
-location ~ \.php$ {
+
+sudo tee -a /etc/nginx/sites-available/wordpress.conf > /dev/null <<EOT
+server {
+        listen 80;
+        root /var/www/html;
+        index index.php index.html index.htm index.nginx-debian.html;
+       # server_name wordpress-nginx-conf;
+
+        location = /favicon.ico { log_not_found off; access_log off; }
+        location = /robots.txt { log_not_found off; access_log off; allow all; }
+        location ~* \.(css|gif|ico|jpeg|jpg|js|png)$ {
+             expires max;
+             log_not_found off;
+        }
+
+        location / {
+
+                try_files $uri $uri/ /index.php$is_args$args;
+        }
+
+        location ~ \.php$ {
                 include snippets/fastcgi-php.conf;
                 fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
+        }
+
+        #location ~ /\.ht {
+         #       deny all;
+        #}
 }
+
 EOT
+sudo ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
+sudo unlink /etc/nginx/sites-enabled/default
+sudo apt update
+sudo systemctl reload nginx
 sudo systemctl restart nginx.service php7.4-fpm
