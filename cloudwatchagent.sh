ba8c31ac01db6fe2514d033a8e138133fb013e5a
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-05-14 13:45:30 +0530

    Update cloudwatchagent.sh

--- cloudwatchagent.sh
+++ cloudwatchagent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-sudo apt-get update && sudo apt-get install collectd
+sudo apt-get update && sudo apt-get -y install collectd
 
 sudo bash /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
