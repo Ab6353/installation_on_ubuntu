155b517acc2babf423721de722d06e3220a0e900
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-05-25 11:14:33 +0530

    Update minikube.sh

--- k8s minikube/minikube.sh
+++ k8s minikube/minikube.sh
@@ -16,3 +16,6 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plu
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
