1ae828e3d453b421bbfb35b76d169420f4cfc086
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-10-14 12:04:38 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-#downloading and adding the MongoDB key
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-#adding MongoDB repository
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-
+#version 6.0.2
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
-
 sudo apt-get install -y mongodb-org
+
 
 sudo systemctl start mongod
 sudo systemctl enable mongod
