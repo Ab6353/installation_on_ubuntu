81edb24a1f6baa3263a7213cc6c6587a4583762e
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2023-03-16 14:43:02 +0530

    udpated new docker

--- docker.sh
+++ docker.sh
@@ -1,15 +1,14 @@
-#!bin/bash
+#!/bin/bash
 sudo apt-get update
-sudo apt-get install -y \
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
